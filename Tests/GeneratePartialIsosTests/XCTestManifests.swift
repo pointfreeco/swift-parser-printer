7eaed766b610133db12d6ac199cd1fe89c89786b
Brandon Williams <mbw234@gmail.com>
2019-02-15 13:56:45 -0500

    wip

---
+++ Tests/GeneratePartialIsosTests/XCTestManifests.swift
@@ -0,0 +1,15 @@
+import XCTest
+
+extension GeneratePartialIsosTests {
+    static let __allTests = [
+        ("test", test),
+    ]
+}
+
+#if !os(macOS)
+public func __allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(GeneratePartialIsosTests.__allTests),
+    ]
+}
+#endif
